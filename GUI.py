884715204f3d00531ee0968e1dcbb32767f93d29
Anna <amj0851@gmail.com>
2021-04-17 23:19:10 -0400

    wip

--- GUI.py
+++ GUI.py
@@ -1,9 +1,16 @@
 import tkinter
+from tkinter import messagebox
+from tkinter import *
 
 top = tkinter.Tk()
 
+progOne = tkinter.Progressbar(top, orient = HORIZONTAL, length = 100, mode ='determinate')
+
 def OnButtonPress():
 	tkinter.messagebox.showinfo("you killed a turtle")
+	progOne.pack()
+	progOne["value"] += 10
+
 
 B = tkinter.Button(top, text="add plastic", command = OnButtonPress)
 B.pack()
