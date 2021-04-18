579fbe2598f88d356b2183b0808d7d5189529f6f
Smeppitty <dmsolinsky@email.wm.edu>
2021-04-17 22:57:06 -0400

    initial upload

---
+++ src/constants.py
@@ -0,0 +1,2 @@
+def pollution_constants():
+	return {11, 11.0, 5, 6}
---
+++ src/main.py
@@ -0,0 +1,8 @@
+from constants import pollution_constants
+
+def main():
+    l = pollution_constants()
+    print(l)
+
+if __name__ == "__main__":
+    main()
